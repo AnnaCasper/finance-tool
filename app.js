fb486a5425112e11e4de797144eae1ed1629dc61
Anna Casper <anna.casper3@gmail.com>
2017-01-29 11:10:53 -0700

    change procfile

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// var express = require('express')
+// var app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
