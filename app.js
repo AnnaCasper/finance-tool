ca21bfa4b250e1065185828785a247f32508676a
Anna Casper <anna.casper3@gmail.com>
2017-01-30 11:17:01 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-// var express = require('express')
-// var app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
+app.listen(process.env.PORT || 3000);
